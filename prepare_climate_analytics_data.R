8d21d5273d346619522fdf77e4048d7e00089c03
FrederickFa <60888622+FrederickFa@users.noreply.github.com>
2021-07-22 07:52:56 +0200

    refine workflow of climate analytics data

--- prepare_climate_analytics_data.R
+++ prepare_climate_analytics_data.R
@@ -20,9 +20,38 @@ all_countries <- rworldmap::countryRegions %>%
 
 # cross parameters
 api_paramter <- tidyr::crossing(
-  region = all_countries,
+  region = c(
+    "AFRICA",
+    "EUROPE",
+    "NORTH_AMERICA",
+    "SOUTH_AMERICA",
+    "ASIA",
+    "OCEANIA",
+    "ATA"
+
+  ),
   indicator = c(
-    "tasAdjust"
+    "tasAdjust", # air temperature
+    "tasminAdjust", # daily minimum air temperature
+    "tasmaxAdjust", # daily maximum air temperature
+    "prAdjust", # precipitation
+    "hursAdjust", # relative humidity
+     "prsnAdjust", # snowfall
+    "hussAdjust", # specific humidity
+    "sfcWindAdjust", # wind speed
+    "ec4", # 1-in-100-year expected damage from tropical cyclones
+    "ec2", # annual expected damage from river floods
+    "ec3", # annual expected damage from tropical cyclones
+    "ec1", # labour productivity due to heat stress
+    "lec", # land fraction annually exposed to crop failures
+    "leh", # land fraction annually exposed to heat waves
+    "fldfrc", # land fraction annually exposed to river floods
+    "lew", # land fraction annually exposed to wild fires,
+    "flddph", # river flood depth
+    "maxdis", # maximum daily river discharge
+    "mindis", # minimum daily river discharge
+    "dis", # river discharge
+    "qs" # surface runoff
   ),
   scenario = c(
     "cat_current",
@@ -41,7 +70,7 @@ api_paramter <- tidyr::crossing(
   )
 )
 
-# create link
+# create links
 api_paramter <- api_paramter %>%
   dplyr::mutate(
     link = paste0(
@@ -57,219 +86,270 @@ api_paramter <- api_paramter %>%
   )
 
 
-# =================================
-# prep webscraping data
-# =================================
-all_data <- data.frame(
-  region = NA_character_,
-  indicator = NA_character_,
-  scenario = NA_character_,
-  year = NA_integer_,
-  link = NA_character_,
-  variable_id = NA_character_,
-  country = NA_character_,
-  warming_level = NA_character_,
-  reference = NA_character_,
-  difference = NA_character_,
-  gcm_gim_combinations = NA_character_,
-  all_used_runs = NA_character_,
-  lat = NA_character_,
-  long = NA_character_,
-  risk_level = NA_character_
-)
-
+# define starting variables + data frame
 processed_links <- 0
+processed_indicators <- 0
+all_duration <- NA
 
-# =================================
-# scrape data
-# =================================
-for (sub_link in api_paramter$link) {
+for (sub_indicator in unique(api_paramter$indicator)) {
 
   # define start time to get an estimate how much time is needed to load the data
   start_time <- Sys.time()
 
-  # add an estimate of how many links have been downloaded
-  processed_links <- processed_links + 1
-
-  # message which link is currently processed
-  message(paste0("Processing link Nr.", processed_links, " of ", nrow(api_paramter)))
-  print(sub_link)
-
-  # filter api_paramater to get the relevant parameters for each links
+  # subset the links by indicator
   api_paramter_sub <- api_paramter %>%
-    filter(link == sub_link)
+    filter(indicator == sub_indicator)
 
-  # download data
-  data <- vroom::vroom(sub_link, delim = ",")
+  # create target data frame
+  all_data <- data.frame(
+    region = NA_character_,
+    indicator = NA_character_,
+    scenario = NA_character_,
+    year = NA_integer_,
+    link = NA_character_,
+    variable_id = NA_character_,
+    country = NA_character_,
+    warming_level = NA_character_,
+    reference = NA_character_,
+    difference = NA_character_,
+    gcm_gim_combinations = NA_character_,
+    all_used_runs = NA_character_,
+    lat = NA_character_,
+    long = NA_character_,
+    risk_level = NA_character_
+  )
 
-  # in case a set of parameters is not available, the downloaded data will only have one column
-  # --> only proceed data wrangling if set of parameters is available
-  if(ncol(data) == 2) {
+  # count processed hazards
+  processed_indicators <- processed_indicators + 1
 
-    # as names of downloaded data vary based on parameters, create agnostic variables
-    names(data) <- c("v1","v2")
+  # scrape data
+  for (sub_link in api_paramter_sub$link) {
 
-    # slice rows which contain parameter information
-    summary <- data %>%
-      slice(c(1:7))
+    # add an estimate of how many links have been downloaded
+    processed_links <- processed_links + 1
 
-    # prep sumamry of parameter information
-    summary <- summary %>%
-      pivot_wider(id_cols = "v2", names_from = "v1", values_from = "v2") %>%
-      janitor::clean_names(case = "snake")
+    # message which link is currently processed
+    message(paste0("Processing link Nr.", processed_links, " of ", nrow(api_paramter)))
+    print(sub_link)
 
-    # kickout rows which contain parameter information
-    data <- data %>%
-      slice(-c(1:7))
+    # filter api_paramater to get the relevant parameters for each links
+    api_paramter_sub_sub <- api_paramter_sub %>%
+      dplyr::filter(link == sub_link)
 
-    # slice rows which contains the number of longitudes
-    index <- data %>% select(v2) %>% slice(1)
+    # download data
+    data <- vroom::vroom(sub_link, delim = ",")
 
-    # count the number of longitudes
-    index <- stringr::str_count(index$v2, ",")
+    # in case a set of parameters is not available, the downloaded data will only have one column
+    # --> only proceed with data wrangling if set of parameters is available
+    if(ncol(data) == 2) {
 
-    # add 1 to the number of longitudes, as there is always one comma less
-    index <- index + 1
+      # as names of downloaded data vary based on parameters, create agnostic variables
+      names(data) <- c("v1","v2")
 
-    # expand the data
-    data <- data %>%
-      tidyr::separate(v2,into = as.character(c(1:index)),  sep = ",")
+      # slice rows which contain parameter information
+      summary <- data %>%
+        dplyr::slice(c(1:7))
 
-    # use longitude as the column name
-    colnames(data) <- data %>% slice(1)
+      # prep sumamry of parameter information
+      summary <- summary %>%
+        tidyr::pivot_wider(id_cols = "v2", names_from = "v1", values_from = "v2") %>%
+        janitor::clean_names(case = "snake")
 
-    # kickout row which contains the longitude
-    data <- data %>% slice(-c(1))
+      # kickout rows which contain parameter information
+      data <- data %>%
+        slice(-c(1:7))
 
-    # rename latitude
-    data <- data %>%
-      rename(lat = `lat \\ lon`)
+      # slice rows which contains the number of longitudes
+      index <- data %>%
+        dplyr::select(v2) %>%
+        dplyr::slice(1)
 
-    # pivot longer
-    data <- data %>%
-      pivot_longer(cols = !lat, values_to = "risk_level", names_to = "long")
+      # count the number of longitudes
+      index <- stringr::str_count(index$v2, ",")
 
-    # kickout coordinates without risk data (as countries are not squares this is expected to happen)
-    data <- data %>%
-      filter(risk_level != "")
+      # add 1 to the number of longitudes, as there is always one comma less
+      index <- index + 1
 
-    # add summary and paramter information to data
-    data <- bind_cols(
-      api_paramter_sub,
-      summary,
-      data
-    )
+      # expand the data
+      data <- data %>%
+        tidyr::separate(v2,into = as.character(c(1:index)),  sep = ",")
 
-    # bind data from previous loops to all data
-    all_data <- rbind.data.frame(
-      all_data,
-      data
-    )
+      # use longitude as the column name
+      colnames(data) <- data %>%
+        dplyr::slice(1)
+
+      # kickout row which contains the longitude
+      data <- data %>%
+        dplyr::slice(-c(1))
+
+      # rename latitude
+      data <- data %>%
+        dplyr::rename(lat = `lat \\ lon`)
+
+      # pivot longer
+      data <- data %>%
+        tidyr::pivot_longer(cols = !lat, values_to = "risk_level", names_to = "long")
+
+      # kickout coordinates without risk data (as countries are not squares this is expected to happen)
+      data <- data %>%
+        dplyr::filter(risk_level != "")
+
+      # add summary and paramter information to data
+      data <- dplyr::bind_cols(
+        api_paramter_sub_sub,
+        summary,
+        data
+      )
+
+      # bind data from previous loops to all data
+      all_data <- rbind.data.frame(
+        all_data,
+        data
+      )
+    }
+
+    # ensure that there is always a small break between downloading data
+    Sys.sleep(1)
+
   }
 
-  # ensure that there is always a small break between downloading data
-  Sys.sleep(0.2)
+  # kickput out the starting row of the target data frame
+  all_data <- all_data %>%
+    filter(!is.na(link))
 
-  # determine the end time
-  end_time <- Sys.time()
+  if(nrow(all_data > 0)) {
+    # apply distinct to eliminate duplicate entries for the same centroids used at the borders of to regions / countries
+    all_data <- all_data %>%
+      distinct(scenario, indicator, year, long, lat, .keep_all = TRUE)
 
-  # calculate how long it took
-  duration <- as.double(end_time) - as.double(start_time)
+    # duplicate long lats for sf coordinates
+    all_data <- all_data %>%
+      mutate(
+        duplicate_long = long,
+        duplicate_lat = lat
+      )
 
-  # estimate how many minutes are left based on current processing time
-  minutes_left <- round(((nrow(api_paramter)) - processed_links)*duration/60,0)
+    # create sf coordinates / points
+    all_data <- all_data %>%
+      sf::st_as_sf(coords = c("duplicate_long","duplicate_lat"))
 
-  # print estimated processing time which is left
-  message("Roughly ", minutes_left, " minutes left")
+    # hash geometry
+    all_data <- all_data %>%
+      dplyr::mutate(geometry_id = openssl::md5(as.character(geometry)))
 
+
+    # to do: verify counted grouped lat longs equals distinct lat longs
+
+
+    # create sf data frame based on longitude and latitude
+    all_data_distinct_geo_data <- all_data %>%
+      sf::st_drop_geometry() %>%
+      distinct(long, lat, geometry_id) %>%
+      as_tibble()
+
+    all_data_distinct_geo_data <- all_data_distinct_geo_data %>%
+      mutate(
+        long = as.numeric(long),
+        lat = as.numeric(lat),
+      ) %>%
+      mutate(
+        geometry_id = as.character(geometry_id),
+        north_lat = lat + 0.5,
+        south_lat = lat,
+        east_lng = long + 0.5,
+        west_lng = long
+        # north_lat = lat + 0.5/2,
+        # south_lat = lat - 0.5/2,
+        # east_lng = long + 0.5/2,
+        # west_lng = long - 0.5/2
+      ) %>%
+      as.data.frame()
+
+    all_data_distinct_geo_data_polygons <- lapply(
+      1:nrow(all_data_distinct_geo_data), function(x) {
+        ## create a matrix of coordinates that also 'close' the polygon
+        res <- matrix(c(all_data_distinct_geo_data[x, 'west_lng'], all_data_distinct_geo_data[x, 'north_lat'],
+                        all_data_distinct_geo_data[x, 'east_lng'], all_data_distinct_geo_data[x, 'north_lat'],
+                        all_data_distinct_geo_data[x, 'east_lng'], all_data_distinct_geo_data[x, 'south_lat'],
+                        all_data_distinct_geo_data[x, 'west_lng'], all_data_distinct_geo_data[x, 'south_lat'],
+                        all_data_distinct_geo_data[x, 'west_lng'], all_data_distinct_geo_data[x, 'north_lat'])  ## need to close the polygon
+                      , ncol =2, byrow = T
+        )
+        ## create polygon objects
+        st_polygon(list(res))
+
+      }
+    )
+
+    all_data_distinct_geo_data <- st_sf(all_data_distinct_geo_data, st_sfc(all_data_distinct_geo_data_polygons), crs = 4326)
+
+    asset_scenario_data <- sf::st_join(distinct_geo_data, all_data_distinct_geo_data)
+
+    asset_scenario_data <- asset_scenario_data %>%
+      filter(!is.na(geometry_id))
+
+    asset_geometry <- asset_scenario_data %>%
+      select(geometry)
+
+    scenario_geometry <- asset_scenario_data %>%
+      sf::st_drop_geometry() %>%
+      sf::st_as_sf(coords = c("long","lat")) %>%
+      select(geometry)
+
+    sf::st_crs(scenario_geometry) <- 4326
+
+    asset_scenario_data$dist <- sf::st_distance(asset_geometry, scenario_geometry, by_element=TRUE)
+
+    asset_scenario_data <- asset_scenario_data %>%
+      sf::st_drop_geometry() %>%
+      select(asset_id, geometry_id)
+
+
+    asset_scenario_data <- asset_scenario_data %>%
+      left_join(
+        all_data %>%
+          mutate(geometry_id = as.character(geometry_id)),
+        by = c("geometry_id")
+      )
+
+    asset_scenario_data <- asset_scenario_data %>%
+      transmute(
+        asset_id,
+        provider = "ClimateAnalytics",
+        hazard = indicator,
+        scenario,
+        period = year,
+        model = "Ensemble",
+        relative_change = risk_level,
+        risk_level = NA,
+        reference = NA,
+        absolute_change = NA,
+        geometry_id
+      )
+
+
+    asset_scenario_data %>%
+      save_climate_data(
+        path_db_pr_climate_data_provider = fs::path(path_db_pr_climate_data, "ClimateAnalytics"),
+        use_distinct_for_assets_between_two_rasters = TRUE,
+        drop_any_NAs = FALSE
+      )
+
+    # determine the end time
+    end_time <- Sys.time()
+
+    # calculate how long it took
+    duration <- as.double(end_time) - as.double(start_time)
+    all_duration <- c(all_duration, duration)
+
+    # estimate how many minutes are left based on current processing time
+    minutes_left <- round((length(unique(api_paramter$indicator)) - processed_indicators)*mean(all_duration, na.rm = TRUE)/60,0)
+
+    # print estimated processing time which is left
+    message("Roughly ", minutes_left, " minutes left")
+  }
+
+  Sys.sleep(2)
 }
 
-all_data <- all_data %>%
-  filter(!is.na(link))
 
-# dupliactes for country
-all_data <- all_data %>%
-  distinct(scenario, indicator, year, long, lat, .keep_all = TRUE)
-
-all_data_test <- all_data %>%
-  mutate(
-    duplicate_long = long,
-    duplicate_lat = lat
-  )
-
-
-all_data_test <- all_data_test %>%
-  sf::st_as_sf(coords = c("duplicate_long","duplicate_lat"))
-
-all_data_test <- all_data_test %>%
-  dplyr::mutate(geometry_id = openssl::md5(as.character(geometry)))
-
-
-# to do: verify counted grouped lat longs equals distinct lat longs
-
-
-# create sf data frame based on longitude and latitude
-all_data_distinct_geo_data <- all_data_test %>%
-  sf::st_drop_geometry() %>%
-  distinct(long, lat, geometry_id) %>%
-  mutate(
-    duplicate_long = long,
-    duplicate_lat = lat
-  ) %>%
-  as_tibble()
-
-all_data_distinct_geo_data <- sf::st_as_sf(all_data_distinct_geo_data, coords = c("duplicate_long","duplicate_lat"))
-
-# assign crs to enable intersecting
-sf::st_crs(all_data_distinct_geo_data) <- 4326
-
-test <- sf::st_join(distinct_geo_data, all_data_distinct_geo_data, sf::st_nearest_feature)
-
-
-asset_geometry <- test %>%
-  select(geometry)
-
-
-scenario_geometry <- test %>%
-  sf::st_drop_geometry() %>%
-  sf::st_as_sf(coords = c("long","lat")) %>%
-  select(geometry)
-
-sf::st_crs(scenario_geometry) <- 4326
-
-
-dist = sf::st_distance(distinct_geo_data, all_data_distinct_geo_data[nearest,], by_element=TRUE)
-
-test$dist <- sf::st_distance(asset_geometry, scenario_geometry, by_element=TRUE)
-
-test <- test %>%
-  sf::st_drop_geometry() %>%
-  select(asset_id, geometry_id)
-
-
-test <- test %>%
-  left_join(all_data_test, by = c("geometry_id"))
-
-
-test <- test %>%
-  transmute(
-    asset_id,
-    provider = "ClimateAnalytics",
-    hazard = indicator,
-    scenario,
-    period = year,
-    model = gcm_gim_combinations,
-    relative_change = risk_level,
-    risk_level = NA,
-    reference = NA,
-    absolute_change = NA,
-    geometry_id
-  )
-
-
-test %>%
-  save_climate_data(
-    path_db_pr_climate_data_provider = fs::path(path_db_pr_climate_data, "ClimateAnalytics"),
-    use_distinct_for_assets_between_two_rasters = TRUE,
-    drop_any_NAs = FALSE
-  )
