64636d90ee9b51dc2523f6e3a56123d5313a4040
FrederickFa <60888622+FrederickFa@users.noreply.github.com>
2021-07-23 19:21:45 +0200

    add different allocation methods

--- analysis.R
+++ analysis.R
@@ -80,42 +80,10 @@ for (portfolio in unique(eq_portfolio$portfolio_name)) {
       by = "target_company_id"
     )
 
-
-
-  # ========
-  # add final indicators
-  # ========
-
-  # calculate portfolio_economic_value
-  analysis <- analysis %>%
-    mutate(portfolio_economic_value = if_else(sector == security_mapped_sector, ownership_weight*company_final_owned_economic_value, 0))
-
-  # calculate portfolio_economic_value_share_technology
-  analysis <- analysis %>%
-    group_by(portfolio_name, provider, hazard, model, period, sector, technology, year) %>%
-    mutate(portfolio_economic_value_share_technology = portfolio_economic_value / sum(portfolio_economic_value, na.rm = T)) %>%
-    ungroup()
-
-  # calculate portfolio_economic_value_share_technology_company
-  analysis <- analysis %>%
-    group_by(portfolio_name, provider, company_name, hazard, model, period, sector, technology, year) %>%
-    mutate(portfolio_economic_value_share_technology_company = portfolio_economic_value / sum(portfolio_economic_value, na.rm = T)) %>%
-    ungroup()
-
-  # calculate portfolio_economic_value_share_sector
-  analysis <- analysis %>%
-    group_by(portfolio_name, provider, hazard, model, period, sector, year) %>%
-    mutate(portfolio_economic_value_share_sector = portfolio_economic_value / sum(portfolio_economic_value, na.rm = T)) %>%
-    ungroup()
-
-  # calculate portfolio_economic_value_share_sector_company
-  analysis <- analysis %>%
-    group_by(portfolio_name, provider, company_name, hazard, model, period, sector, year) %>%
-    mutate(portfolio_economic_value_share_sector_company = portfolio_economic_value / sum(portfolio_economic_value, na.rm = T)) %>%
-    ungroup()
-
-
+  # =================================
   # create overview stats
+  # =================================
+
   has_ald <- analysis %>%
     filter(!is.na(asset_id)) %>%
     distinct(portfolio_name, company_name, asset_id) %>%
@@ -146,74 +114,120 @@ for (portfolio in unique(eq_portfolio$portfolio_name)) {
   eq_portfolio_sub %>%
     plot_portfolio_geo_ald_value()
 
-  save_overview_plot(name = "portfolio_geo_ald_value", path = path_db_pacta_project_pr_output_equity_portfolio)
+  save_overview_plot(name = "portfolio_geo_ald_value", path = path_db_pacta_project_pr_output_equity_portfolio_allocation)
 
   # plot overview stats
   eq_portfolio_sub %>%
     plot_portfolio_geo_ald_holdings()
 
-  save_overview_plot(name = "portfolio_geo_ald_holdings", path = path_db_pacta_project_pr_output_equity_portfolio)
+  save_overview_plot(name = "portfolio_geo_ald_holdings", path = path_db_pacta_project_pr_output_equity_portfolio_allocation)
 
-  # plot results
-  path_db_pacta_project_pr_output_equity_portfolio_plots <- fs::path(path_db_pacta_project_pr_output_equity_portfolio, "plots")
 
-  analysis %>%
-    mutate(is_reference_period = dplyr::if_else(period == "1991-2020", TRUE, FALSE)) %>%
-    filter(is_reference_period == FALSE) %>%
-    filter(security_mapped_sector == sector) %>%
-    for_loops_climate_data(
-      parent_path = fs::path(path_db_pacta_project_pr_output_equity_portfolio_plots),
-      fns = function(data, final_path) {
+  # =================================
+  # add final indicators
+  # =================================
 
-        ####### asset_risk_histgram
-        asset_risk_histgram <- data %>%
-          plot_asset_risk_histgram() +
-          scale_fill_relative_risk()
+  for (allocation in c("port_weight", "ownership")) {
+    path_db_pacta_project_pr_output_equity_portfolio_allocation <- fs::path(path_db_pacta_project_pr_output_equity_portfolio, allocation)
 
-        save_result_plot(name = "asset_risk_histgram", path = final_path)
+    create_db_pr_paths(paths = path_db_pacta_project_pr_output_equity_portfolio_allocation)
 
-        ####### company_risk_distribution
-        company_risk_distribution <- data %>%
-          plot_company_risk_distribution() +
-          scale_fill_relative_risk()
 
-        save_result_plot(name = "company_risk_distribution", path = final_path)
+    if (allocation == "ownership") {
+      # calculate portfolio_economic_value using ownership
+      analysis <- analysis %>%
+        mutate(portfolio_economic_value = if_else(sector == security_mapped_sector, ownership_weight*company_final_owned_economic_value, 0))
+    } else if (allocation == "port_weight") {
+      # calculate portfolio_economic_value using port weight
+      analysis <- analysis %>%
+        mutate(portfolio_economic_value = if_else(sector == security_mapped_sector, port_weight*company_final_owned_economic_value, 0))
+    }
 
-        ####### portfolio_company_risk_distribution
-        portfolio_company_risk_distribution <- data %>%
-          plot_portfolio_company_risk_distribution() +
-          scale_fill_relative_risk()
+    # calculate portfolio_economic_value_share_technology
+    analysis <- analysis %>%
+      group_by(portfolio_name, provider, hazard, model, period, sector, technology, year) %>%
+      mutate(portfolio_economic_value_share_technology = portfolio_economic_value / sum(portfolio_economic_value, na.rm = T)) %>%
+      ungroup()
 
-        save_result_plot(name = "portfolio_company_risk_distribution", path = final_path)
+    # calculate portfolio_economic_value_share_technology_company
+    analysis <- analysis %>%
+      group_by(portfolio_name, provider, company_name, hazard, model, period, sector, technology, year) %>%
+      mutate(portfolio_economic_value_share_technology_company = portfolio_economic_value / sum(portfolio_economic_value, na.rm = T)) %>%
+      ungroup()
 
-        ####### number_of_assets
-        number_of_assets <- data %>%
-          plot_sector_number_of_assets() +
-          scale_fill_relative_risk()
+    # calculate portfolio_economic_value_share_sector
+    analysis <- analysis %>%
+      group_by(portfolio_name, provider, hazard, model, period, sector, year) %>%
+      mutate(portfolio_economic_value_share_sector = portfolio_economic_value / sum(portfolio_economic_value, na.rm = T)) %>%
+      ungroup()
 
-        save_result_plot(name = "number_of_assets", path = final_path)
+    # calculate portfolio_economic_value_share_sector_company
+    analysis <- analysis %>%
+      group_by(portfolio_name, provider, company_name, hazard, model, period, sector, year) %>%
+      mutate(portfolio_economic_value_share_sector_company = portfolio_economic_value / sum(portfolio_economic_value, na.rm = T)) %>%
+      ungroup()
 
-        ####### relative_sector_production
-        relative_sector_production <- data %>%
-          plot_sector_relative_portfolio_economic_value() +
-          scale_fill_relative_risk()
+    # plot results
+    path_db_pacta_project_pr_output_equity_portfolio_plots <- fs::path(path_db_pacta_project_pr_output_equity_portfolio_allocation, "plots")
 
-        save_result_plot(name = "relative_sector_production", path = final_path)
+    analysis %>%
+      mutate(is_reference_period = dplyr::if_else(period == "1991-2020", TRUE, FALSE)) %>%
+      filter(is_reference_period == FALSE) %>%
+      filter(security_mapped_sector == sector) %>%
+      for_loops_climate_data(
+        parent_path = fs::path(path_db_pacta_project_pr_output_equity_portfolio_plots),
+        fns = function(data, final_path) {
 
-        ####### absolute_sector_production
-        absolute_sector_production <- data %>%
-          plot_sector_absolute_portfolio_economic_value() +
-          scale_fill_relative_risk()
+          ####### asset_risk_histgram
+          asset_risk_histgram <- data %>%
+            plot_asset_risk_histgram() +
+            scale_fill_relative_risk()
 
-        save_result_plot(name = "absolute_sector_production", path = final_path)
+          save_result_plot(name = "asset_risk_histgram", path = final_path)
 
-      }
-    )
+          ####### company_risk_distribution
+          company_risk_distribution <- data %>%
+            plot_company_risk_distribution() +
+            scale_fill_relative_risk()
 
-  analysis %>%
-    filter(security_mapped_sector == sector) %>%
-    readr::write_csv(
-      fs::path(path_db_pacta_project_pr_output_equity_portfolio, "results", ext = "csv")
-    )
+          save_result_plot(name = "company_risk_distribution", path = final_path)
+
+          ####### portfolio_company_risk_distribution
+          portfolio_company_risk_distribution <- data %>%
+            plot_portfolio_company_risk_distribution() +
+            scale_fill_relative_risk()
+
+          save_result_plot(name = "portfolio_company_risk_distribution", path = final_path)
+
+          ####### number_of_assets
+          number_of_assets <- data %>%
+            plot_sector_number_of_assets() +
+            scale_fill_relative_risk()
+
+          save_result_plot(name = "number_of_assets", path = final_path)
+
+          ####### relative_sector_production
+          relative_sector_production <- data %>%
+            plot_sector_relative_portfolio_economic_value() +
+            scale_fill_relative_risk()
+
+          save_result_plot(name = "relative_sector_production", path = final_path)
+
+          ####### absolute_sector_production
+          absolute_sector_production <- data %>%
+            plot_sector_absolute_portfolio_economic_value() +
+            scale_fill_relative_risk()
+
+          save_result_plot(name = "absolute_sector_production", path = final_path)
+
+        }
+      )
+
+    analysis %>%
+      filter(security_mapped_sector == sector) %>%
+      readr::write_csv(
+        fs::path(path_db_pacta_project_pr_output_equity_portfolio_allocation, "results", ext = "csv")
+      )
+  }
 
 }
